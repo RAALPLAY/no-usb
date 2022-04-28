62d7809e0c5e078cd9433a4c975a88af43095435
RAALPLAY <84200771+RAALPLAY@users.noreply.github.com>
2022-04-28 12:27:22 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-
-//busca la url en update.xml
-  $(document).ready(function(){
-    $.get("update.xml",{},function(xml){
-      // alert(xml);
-      $('update',xml).each(function(i) {
-        var url = ((xml.getElementsByTagName('link')[i]).childNodes[0]).nodeValue;
-        document.getElementById("download").href = url
-      });
-    });
-  });
-
 //inicia la animacion wave.json
-var animation = bodymovin.loadAnimation({
+  var animation = bodymovin.loadAnimation({
     container: document.getElementById('bm'),
     renderer: 'svg',
     loop: true,
